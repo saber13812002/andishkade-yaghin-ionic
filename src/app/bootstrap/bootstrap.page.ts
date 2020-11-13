01ba045ae2ab997a3a1d423020493e11d6756670
saber.tabatabaee <saber.tabatabaee@gmail.com>
2020-11-13 17:40:23 +0330

    get token from localstorage

--- src/app/bootstrap/bootstrap.page.ts
+++ src/app/bootstrap/bootstrap.page.ts
@@ -4,6 +4,7 @@ import { LoadingController, NavController, } from '@ionic/angular';
 import { AlertService } from '../services/alert.service';
 import { ToastService } from '../services/toast.service';
 import { ApiService } from '../services/api.service';
+import { Alert } from '../models/alert';
 
 @Component({
   selector: 'app-bootstrap',
@@ -15,10 +16,10 @@ export class BootstrapPage implements OnInit {
   token;
 
   constructor(public navCtrl: NavController,
-    public loadingCtrl: LoadingController,
-    public alertService: AlertService,
-    public toastService: ToastService,
-    public apiService: ApiService,
+    private loadingCtrl: LoadingController,
+    private alertService: AlertService,
+    private toastService: ToastService,
+    private apiService: ApiService,
     private router: Router,
   ) { }
 
@@ -27,28 +28,36 @@ export class BootstrapPage implements OnInit {
     this.token = localStorage.getItem('token');
 
     if (this.token != null) {
-
+      this.validateToken(this.token);
     }
-    this.validateToken(this.token);
+    else {
+      localStorage.clear();
+      this.router.navigateByUrl('/login');
+    }
   }
 
   async validateToken(jwt: string) {
+    const loading = await this.loadingCtrl.create({
+      message: ''
+    });
+    await loading.present();
     try {
-      await this.apiService.me("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0IiwiaWF0IjoxNjA1MjU5NjEwLCJleHAiOjE2MDU4NjQ0MTB9.DgUJ5pg7AZZfw4aZXqrCxuBQlY-covLnG3XaFuOdcHVUDrqzLXgIM39UzSislzjs7w3Q3UNUWPT66rGNwQjquw").subscribe(data => {
+      await this.apiService.me(this.token).subscribe(data => {
         console.log('data:', data);
         localStorage.setItem('user', JSON.stringify(data));
+        this.router.navigateByUrl('/home');
       });
       await this.toastService.presentToast("Login successfull!");
-      this.router.navigateByUrl('/home');
-      
       // await this.alertService.present(200);
     } catch (e) {
       // fixme when login page created (remove me)
+      localStorage.clear();
       this.router.navigateByUrl('/login');
       console.log(e);
-      await this.alertService.present(e.code);
     }
+    await loading.dismiss();
   }
+
 
   async login(credential) {
     await this.apiService.login(credential).subscribe(data => {
