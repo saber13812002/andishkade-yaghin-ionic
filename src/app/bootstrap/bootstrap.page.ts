c56ea24342b67cf4e53ed81f2e860b10fbc73dec
saber.tabatabaee <saber.tabatabaee@gmail.com>
2020-11-13 15:14:44 +0330

    bootstrap page > need to call me api with stored fake token and if successful goto home page

--- src/app/bootstrap/bootstrap.page.ts
+++ src/app/bootstrap/bootstrap.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadingController, NavController, NavParams, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { LoadingController, NavController, } from '@ionic/angular';
+import { AlertService } from '../services/alert.service';
+import { ToastService } from '../services/toast.service';
 import { ApiService } from '../services/api.service';
 
 @Component({
@@ -13,9 +16,11 @@ export class BootstrapPage implements OnInit {
 
   constructor(public navCtrl: NavController,
     public loadingCtrl: LoadingController,
-    public toastController: ToastController,
-    public apiService:ApiService,
-    ) { }
+    public alertService: AlertService,
+    public toastService: ToastService,
+    public apiService: ApiService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
 
@@ -28,7 +33,25 @@ export class BootstrapPage implements OnInit {
   }
 
   async validateToken(jwt: string) {
-    await this.apiService.getList().subscribe(data => {
+    try {
+      await this.apiService.me("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0IiwiaWF0IjoxNjA1MjU5NjEwLCJleHAiOjE2MDU4NjQ0MTB9.DgUJ5pg7AZZfw4aZXqrCxuBQlY-covLnG3XaFuOdcHVUDrqzLXgIM39UzSislzjs7w3Q3UNUWPT66rGNwQjquw").subscribe(data => {
+        console.log('data:', data);
+        localStorage.setItem('user', JSON.stringify(data));
+      });
+      await this.toastService.presentToast("Login successfull!");
+      this.router.navigateByUrl('/home');
+      
+      // await this.alertService.present(200);
+    } catch (e) {
+      // fixme when login page created (remove me)
+      this.router.navigateByUrl('/login');
+      console.log(e);
+      await this.alertService.present(e.code);
+    }
+  }
+
+  async login(credential) {
+    await this.apiService.login(credential).subscribe(data => {
       console.log(data);
     });
   }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,88 +3,129 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Student } from '../models/student';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
+import { ENVService } from './env.service';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-// API path
-base_path = 'https://masjedcloob.ir/blog/wp-json/wp/v2/posts';
+  constructor(
+    private http: HttpClient,
+    public ENV: ENVService,
+  ) { }
 
-constructor(private http: HttpClient) { }
-
-// Http Options
-httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json'
-  })
-}
-
-// Handle API errors
-handleError(error: HttpErrorResponse) {
-  if (error.error instanceof ErrorEvent) {
-    // A client-side or network error occurred. Handle it accordingly.
-    console.error('An error occurred:', error.error.message);
-  } else {
-    // The backend returned an unsuccessful response code.
-    // The response body may contain clues as to what went wrong,
-    console.error(
-      `Backend returned code ${error.status}, ` +
-      `body was: ${error.error}`);
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
   }
-  // return an observable with a user-facing error message
-  return throwError(
-    'Something bad happened; please try again later.');
-};
 
 
-// Create a new item
-createItem(item): Observable<Student> {
-  return this.http
-    .post<Student>(this.base_path, JSON.stringify(item), this.httpOptions)
-    .pipe(
-      retry(2),
-      catchError(this.handleError)
-    )
+  // Handle API errors
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      // console.error(
+      //   `Backend returned code ${error.status}, ` +
+      //   `body was: ${error.error}`);
+      console.log(error);
+      if (error.status == 401) {
+        return throwError(
+          { 'code': 401 }
+        );
+      }
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+
+
+  // Get login user by token
+  me(token): Observable<User> {
+    let httpOptionsWithToken = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        // TODO: test when token is invalid
+        'authorization': 'Bearer d' + token
+      })
+    }
+
+    return this.http
+      .get<User>(this.ENV.me(), httpOptionsWithToken)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+
+  // Login
+  login(credential): Observable<User> {
+    return this.http
+      .post<User>(this.ENV.login(), JSON.stringify(credential), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+
+  // Create a new item
+  createItem(item): Observable<Student> {
+    return this.http
+      .post<Student>(this.ENV.login(), JSON.stringify(item), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  // Get single student data by ID
+  getItem(id): Observable<Student> {
+    return this.http
+      .get<Student>(this.ENV.login() + '/' + id)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  // Get students data
+  getList(): Observable<Student> {
+    return this.http
+      .get<Student>(this.ENV.login())
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  // Update item by id
+  updateItem(id, item): Observable<Student> {
+    return this.http
+      .put<Student>(this.ENV.login() + '/' + id, JSON.stringify(item), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  // Delete item by id
+  deleteItem(id) {
+    return this.http
+      .delete<Student>(this.ENV.login() + '/' + id, this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
 }
-
-// Get single student data by ID
-getItem(id): Observable<Student> {
-  return this.http
-    .get<Student>(this.base_path + '/' + id)
-    .pipe(
-      retry(2),
-      catchError(this.handleError)
-    )
-}
-
-// Get students data
-getList(): Observable<Student> {
-  return this.http
-    .get<Student>(this.base_path)
-    .pipe(
-      retry(2),
-      catchError(this.handleError)
-    )
-}
-
-// Update item by id
-updateItem(id, item): Observable<Student> {
-  return this.http
-    .put<Student>(this.base_path + '/' + id, JSON.stringify(item), this.httpOptions)
-    .pipe(
-      retry(2),
-      catchError(this.handleError)
-    )
-}
-
-// Delete item by id
-deleteItem(id) {
-  return this.http
-    .delete<Student>(this.base_path + '/' + id, this.httpOptions)
-    .pipe(
-      retry(2),
-      catchError(this.handleError)
-    )
-}}
